fbb9e58aecafc5b9c77cf5d86aa4835931d3f470
Kirill9696 <146858215+Kirill9696@users.noreply.github.com>
2024-01-02 19:57:14 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,1 @@
-let textInput = document.querySelector("#text");
-let buttonNode = document.querySelector(`#button`);
-let resultNode = document.querySelector(`#result`);
-buttonNode.addEventListener(`click`, function () {
-    resultNode.value = `Привет, ` + textInput.value;
-})
+
